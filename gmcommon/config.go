338b349866cb9f639042efcc802286c88b96eab5
Vitor Falcao <vitorfhcosta@gmail.com>
2019-11-16 21:43:30 -0300

    Adds config.json support

---
+++ gmcommon/config.go
@@ -0,0 +1,57 @@
+package gmcommon
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"path"
+)
+
+// Configuration defines the configuration for the engine,
+// it is loaded from a config.json file
+type Configuration struct {
+	Fullscreen bool
+	Scale      float64
+	Width      int
+	Height     int
+}
+
+// ConfigBasePath stores the base path of where to find
+// the config.json file
+var ConfigBasePath string
+
+func init() {
+	path := filepath.Dir(os.Args[0])
+	var err error
+	ConfigBasePath, err = filepath.Abs(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Configuration base path:", ConfigBasePath)
+}
+
+// LoadConfigurations loads all configurations from config.json
+// and makes a Configuration struct from it
+func LoadConfigurations() *Configuration {
+	log.Println("Loading configurations")
+
+	filePath := path.Join(ConfigBasePath, "config.json")
+	file, err := ioutil.ReadFile(filePath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var config Configuration
+	err = json.Unmarshal(file, &config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &config
+}
--- main.go
+++ main.go
@@ -5,18 +5,29 @@ import (
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/vitorfhc/GoMario/gmcommon"
 )
 
 func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
+
 	ebitenutil.DebugPrint(screen, "Hello, World!")
+
 	return nil
 }
 
 func main() {
-	err := ebiten.Run(update, 1600, 900, 1, "Game")
+	config := gmcommon.LoadConfigurations()
+
+	if config.Fullscreen {
+		config.Width, config.Height = ebiten.ScreenSizeInFullscreen()
+		config.Scale = ebiten.DeviceScaleFactor()
+		ebiten.SetFullscreen(true)
+	}
+
+	err := ebiten.Run(update, config.Width, config.Height, config.Scale, "GoMario")
 	if err != nil {
 		log.Fatal(err)
 	}
